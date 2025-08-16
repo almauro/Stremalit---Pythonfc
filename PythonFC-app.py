29b9e63c55777015a4a0b37b5d10d1c2424ddb8b
Mauricio Lozano <57682884+almauro@users.noreply.github.com>
2025-08-16 01:43:28 -0500

    Archivos principales para analizar

---
+++ PythonFC-app.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import io
+import streamlit as st
+
+# Importación de datos con separador correcto
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file, sep=';')
+    
+    # Cálculo de minutos en segmentos de 90
+    df['90s'] = df['minutes']/90
+
+    # Métricas por 90 minutos
+    calc_elements = ['goals', 'assists', 'points']
+    for each in calc_elements:
+        df[f'{each}_p90'] = df[each] / df['90s']
+
+    # Listas únicas para filtros
+    positions = list(df['position'].drop_duplicates())
+    teams = list(df['team'].drop_duplicates())
+
+    # Barra lateral - títulos y filtros
+    st.sidebar.markdown('### Data Filters')
+    position_choice = st.sidebar.multiselect(
+        'Choose position:', positions, default=positions)
+    teams_choice = st.sidebar.multiselect(
+        "Teams:", teams, default=teams)
+    price_choice = st.sidebar.slider(
+        'Max Price:', min_value=4.0, max_value=15.0, step=.5, value=15.0)
+
+    # Aplicar filtros
+    df = df[df['position'].isin(position_choice)]
+    df = df[df['team'].isin(teams_choice)]
+    df = df[df['cost'] < price_choice]
+
+    # Título principal
+    st.title(f"Fantasy Football Analysis")
+
+    # Tabla de jugadores
+    st.markdown('### Player Dataframe')
+    st.dataframe(df.sort_values('points',
+                 ascending=False).reset_index(drop=True))
+
+    # Gráfico costo vs puntos
+    st.markdown('### Cost vs 20/21 Points')
+    st.vega_lite_chart(df, {
+        'mark': {'type': 'circle', 'tooltip': True},
+        'encoding': {
+            'x': {'field': 'cost', 'type': 'quantitative'},
+            'y': {'field': 'points', 'type': 'quantitative'},
+            'color': {'field': 'position', 'type': 'nominal'},
+            'tooltip': [{"field": 'name', 'type': 'nominal'}, {'field': 'cost', 'type': 'quantitative'}, {'field': 'points', 'type': 'quantitative'}],
+        },
+        'width': 700,
+        'height': 400,
+    })
+
+    # Gráfico goles por 90 vs asistencias por 90
+    st.markdown('### Goals p90 vs Assists p90')
+    st.vega_lite_chart(df, {
+        'mark': {'type': 'circle', 'tooltip': True},
+        'encoding': {
+            'x': {'field': 'goals_p90', 'type': 'quantitative'},
+            'y': {'field': 'assists_p90', 'type': 'quantitative'},
+            'color': {'field': 'position', 'type': 'nominal'},
+            'tooltip': [{"field": 'name', 'type': 'nominal'}, {'field': 'cost', 'type': 'quantitative'}, {'field': 'points', 'type': 'quantitative'}],
+        },
+        'width': 700,
+        'height': 400,
+    })
+else:
+    st.warning("Please upload a CSV file.")
